eb6dec86350b29570c6cdd36fcd246043796fe63
Z-Stanislav <master199@gmail.com>
2023-03-02 21:57:09 +0300

    Решил задачи

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// Console.Write("Введите трехзначное число");
+// int Value = Convert.ToInt32(Console.ReadLine());
+// int Length = Value.ToString().Length;
+// if (Length == 3)
+// {
+//     int result = (Value / 10) % 10;
+//     Console.WriteLine("Вторая цифра " + result);
+// }
+// else
+// {
+//     Console.WriteLine("Ввели не трехзначное число");
+// }
+
+
+
+// Console.Write("Ввелите цифру:");
+// int Value2 = Convert.ToInt32(Console.ReadLine());
+// int Length2 = Value2.ToString().Length;
+// if (Length2 >= 3)
+// {
+//     while (Value2 > 999)
+//     {
+//         Value2 = Value2 / 10;
+//     }
+//     int result = Value2 % 10;
+//     Console.WriteLine("Третья цифра " + result);
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет.");
+// }
+
+
+// Console.Write("Введите цифру обозначающую день недели от 1 до 7: ");
+// int Day = Convert.ToInt32(Console.ReadLine());
+// if (Day >= 1 && Day <= 7)
+// {
+//     if (Day >= 6)
+//     {
+//         Console.Write("Выходной день");
+//     }
+//     else
+//     {
+//         Console.Write("Рабочий день");
+//     }
+// }
+// else
+// {
+//     Console.Write("Число не соответствует задачи");
+// }
